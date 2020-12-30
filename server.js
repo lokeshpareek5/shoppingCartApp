19aa2a94bca4168ec39d3281cad85eed490451d5
Lokesh Pareek <lokeshpareek5@users.noreply.github.com>
2020-12-30 14:41:52 +0530

    update for deployment on heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/shopping-cart-app"));
+app.use(express.static(__dirname + "/dist/shoppingCartApp"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/shopping-cart-app/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/shoppingCartApp/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
